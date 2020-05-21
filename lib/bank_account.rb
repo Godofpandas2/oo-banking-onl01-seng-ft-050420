39454d30020468745210a5d55de921f2832b3095
David Hutchison <dhutchis68516@gmail.com>
2020-05-21 17:03:46 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -7,4 +7,20 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
+
+  def deposit
+
+  end
+
+  def display_balance
+
+  end
+
+  def valid?
+
+  end
+
+  def close_account
+
+  end
 end
