4429dccf08eae7664ff2dfe9de76bb0380b520ab
David Hutchison <dhutchis68516@gmail.com>
2020-05-21 17:01:31 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,10 @@
 class BankAccount
   attr_reader :name
-  attr_writer :balance
+  attr_writer :balance, :open
 
   def initialize(name)
     @name = name
     @balance = 1000
-
+    @status = "open"
   end
 end
